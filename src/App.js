037c4def6fccebf4ee079c08f84c0f7c2ed93b05
sagar naikele <sagarnaikele@gmail.com>
2023-02-21 15:14:56 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './NavigationBar/navigationBar';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
